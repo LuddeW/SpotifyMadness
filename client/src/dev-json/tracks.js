2658793c44ea5dafdc2ff04663cd3748f2bef92e
Hektor Wallin <hektorw@gmail.com>
2018-03-27 22:06:17 +0200

    Implemented simple version of battle

--- client/src/dev-json/tracks.js
+++ client/src/dev-json/tracks.js
@@ -1,1 +1,836 @@
-export default []
+export default [
+  {
+    album: {
+      images: [
+        {
+          height: 640,
+          url:
+            'https://i.scdn.co/image/dfc8859cfa4ac0a761742f6b64e45f4ea65ece50',
+          width: 640
+        },
+        {
+          height: 300,
+          url:
+            'https://i.scdn.co/image/5a722077548b7d48abfd93324592026337789a8e',
+          width: 300
+        },
+        {
+          height: 64,
+          url:
+            'https://i.scdn.co/image/955b112723eae662639f2322054b7a1cb8f4ad1a',
+          width: 64
+        }
+      ],
+      name: 'Black Panther The Album Music From And Inspired By'
+    },
+    id: '2Imq77E6CEJR5TPUxlJvTY',
+    name: 'All The Stars (with SZA)',
+    popularity: 93,
+    preview_url:
+      'https://p.scdn.co/mp3-preview/3abbde5549de5ab7c515d58bac2fd4849786d8cb?cid=774b29d4f13844c495f206cafdad9c86'
+  },
+  {
+    album: {
+      images: [
+        {
+          height: 640,
+          url:
+            'https://i.scdn.co/image/c6d179eef98885a3e2f9c50fb61518a3b9274d4b',
+          width: 640
+        },
+        {
+          height: 300,
+          url:
+            'https://i.scdn.co/image/779780d9880e529ab723041f7d98aa585809e2fe',
+          width: 300
+        },
+        {
+          height: 64,
+          url:
+            'https://i.scdn.co/image/a8aa7dde9d982322af36a141c0431ac105f25ec1',
+          width: 64
+        }
+      ],
+      name: 'Pray For Me (with Kendrick Lamar)'
+    },
+    id: '4ofvoCdXq99vpMZO4Cfp8x',
+    name: 'Pray For Me (with Kendrick Lamar)',
+    preview_url:
+      'https://p.scdn.co/mp3-preview/592302e7964f6751ce20c72fd91b2f45577e4236?cid=774b29d4f13844c495f206cafdad9c86'
+  },
+  {
+    album: {
+      images: [
+        {
+          height: 640,
+          url:
+            'https://i.scdn.co/image/c3e9a934880c310249ae24079a11a5f64178423d',
+          width: 640
+        },
+        {
+          height: 300,
+          url:
+            'https://i.scdn.co/image/344f71b1025cd36dc1ec125da27e9cf3f0e81831',
+          width: 300
+        },
+        {
+          height: 64,
+          url:
+            'https://i.scdn.co/image/900fd8513f888783f94e7cf700bd47a255d84564',
+          width: 64
+        }
+      ],
+      name: "King's Dead (with Kendrick Lamar, Future & James Blake)"
+    },
+
+    id: '08qFPu7ksp9KrQJJPuFXxu',
+
+    name: "King's Dead (with Kendrick Lamar, Future & James Blake)",
+
+    preview_url:
+      'https://p.scdn.co/mp3-preview/6a7f67a349b73edb3f2ad8ddbc1ecb026a213077?cid=774b29d4f13844c495f206cafdad9c86'
+  },
+  {
+    album: {
+      images: [
+        {
+          height: 640,
+          url:
+            'https://i.scdn.co/image/a6f7746f54fffaaa3feeabe167e9e669fe2fcd51',
+          width: 640
+        },
+        {
+          height: 300,
+          url:
+            'https://i.scdn.co/image/4e76f9ea1d80c1ed7bbc4e8fe625cf38d66f60cb',
+          width: 300
+        },
+        {
+          height: 64,
+          url:
+            'https://i.scdn.co/image/16556b63635740ec6241455ac6d12651af0ae276',
+          width: 64
+        }
+      ],
+      name: 'DAMN.'
+    },
+
+    id: '5WkPWduQVV8fh7pppm1fqg',
+    name: 'LOVE. FEAT. ZACARI.',
+    preview_url:
+      'https://p.scdn.co/mp3-preview/1922f1d1c6392eb994eb291c96216a9799cb3d1f?cid=774b29d4f13844c495f206cafdad9c86'
+  },
+  {
+    album: {
+      images: [
+        {
+          height: 640,
+          url:
+            'https://i.scdn.co/image/a6f7746f54fffaaa3feeabe167e9e669fe2fcd51',
+          width: 640
+        },
+        {
+          height: 300,
+          url:
+            'https://i.scdn.co/image/4e76f9ea1d80c1ed7bbc4e8fe625cf38d66f60cb',
+          width: 300
+        },
+        {
+          height: 64,
+          url:
+            'https://i.scdn.co/image/16556b63635740ec6241455ac6d12651af0ae276',
+          width: 64
+        }
+      ],
+      name: 'DAMN.'
+    },
+    id: '7ujx3NYtwO2LkmKGz59mXp',
+    name: 'HUMBLE.'
+  },
+  {
+    album: {
+      album_type: 'album',
+      artists: [
+        {
+          external_urls: {
+            spotify: 'https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg'
+          },
+          href: 'https://api.spotify.com/v1/artists/2YZyLoL8N0Wb9xBt1NhZWg',
+          id: '2YZyLoL8N0Wb9xBt1NhZWg',
+          name: 'Kendrick Lamar',
+          type: 'artist',
+          uri: 'spotify:artist:2YZyLoL8N0Wb9xBt1NhZWg'
+        },
+        {
+          external_urls: {
+            spotify: 'https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ'
+          },
+          href: 'https://api.spotify.com/v1/artists/1Xyo4u8uXC1ZmMpatF05PJ',
+          id: '1Xyo4u8uXC1ZmMpatF05PJ',
+          name: 'The Weeknd',
+          type: 'artist',
+          uri: 'spotify:artist:1Xyo4u8uXC1ZmMpatF05PJ'
+        },
+        {
+          external_urls: {
+            spotify: 'https://open.spotify.com/artist/7tYKF4w9nC0nq9CsPZTHyP'
+          },
+          href: 'https://api.spotify.com/v1/artists/7tYKF4w9nC0nq9CsPZTHyP',
+          id: '7tYKF4w9nC0nq9CsPZTHyP',
+          name: 'SZA',
+          type: 'artist',
+          uri: 'spotify:artist:7tYKF4w9nC0nq9CsPZTHyP'
+        }
+      ],
+      available_markets: [
+        'AD',
+        'AR',
+        'AT',
+        'AU',
+        'BE',
+        'BG',
+        'BO',
+        'BR',
+        'CA',
+        'CH',
+        'CL',
+        'CO',
+        'CR',
+        'CY',
+        'CZ',
+        'DE',
+        'DK',
+        'DO',
+        'EC',
+        'EE',
+        'ES',
+        'FI',
+        'FR',
+        'GB',
+        'GR',
+        'GT',
+        'HK',
+        'HN',
+        'HU',
+        'ID',
+        'IE',
+        'IL',
+        'IS',
+        'IT',
+        'JP',
+        'LI',
+        'LT',
+        'LU',
+        'LV',
+        'MC',
+        'MT',
+        'MX',
+        'MY',
+        'NI',
+        'NL',
+        'NO',
+        'NZ',
+        'PA',
+        'PE',
+        'PH',
+        'PL',
+        'PT',
+        'PY',
+        'RO',
+        'SE',
+        'SG',
+        'SK',
+        'SV',
+        'TH',
+        'TR',
+        'TW',
+        'US',
+        'UY',
+        'VN',
+        'ZA'
+      ],
+      external_urls: {
+        spotify: 'https://open.spotify.com/album/5f8pMn2A5d5lKMDapYbCmp'
+      },
+      href: 'https://api.spotify.com/v1/albums/5f8pMn2A5d5lKMDapYbCmp',
+      id: '5f8pMn2A5d5lKMDapYbCmp',
+      images: [
+        {
+          height: 640,
+          url:
+            'https://i.scdn.co/image/dfc8859cfa4ac0a761742f6b64e45f4ea65ece50',
+          width: 640
+        },
+        {
+          height: 300,
+          url:
+            'https://i.scdn.co/image/5a722077548b7d48abfd93324592026337789a8e',
+          width: 300
+        },
+        {
+          height: 64,
+          url:
+            'https://i.scdn.co/image/955b112723eae662639f2322054b7a1cb8f4ad1a',
+          width: 64
+        }
+      ],
+      name: 'Black Panther The Album Music From And Inspired By',
+      release_date: '2018-02-09',
+      release_date_precision: 'day',
+      type: 'album',
+      uri: 'spotify:album:5f8pMn2A5d5lKMDapYbCmp'
+    },
+    artists: [
+      {
+        external_urls: {
+          spotify: 'https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg'
+        },
+        href: 'https://api.spotify.com/v1/artists/2YZyLoL8N0Wb9xBt1NhZWg',
+        id: '2YZyLoL8N0Wb9xBt1NhZWg',
+        name: 'Kendrick Lamar',
+        type: 'artist',
+        uri: 'spotify:artist:2YZyLoL8N0Wb9xBt1NhZWg'
+      },
+      {
+        external_urls: {
+          spotify: 'https://open.spotify.com/artist/0Y5tJX1MQlPlqiwlOH1tJY'
+        },
+        href: 'https://api.spotify.com/v1/artists/0Y5tJX1MQlPlqiwlOH1tJY',
+        id: '0Y5tJX1MQlPlqiwlOH1tJY',
+        name: 'Travis Scott',
+        type: 'artist',
+        uri: 'spotify:artist:0Y5tJX1MQlPlqiwlOH1tJY'
+      }
+    ],
+    available_markets: [
+      'AD',
+      'AR',
+      'AT',
+      'AU',
+      'BE',
+      'BG',
+      'BO',
+      'BR',
+      'CA',
+      'CH',
+      'CL',
+      'CO',
+      'CR',
+      'CY',
+      'CZ',
+      'DE',
+      'DK',
+      'DO',
+      'EC',
+      'EE',
+      'ES',
+      'FI',
+      'FR',
+      'GB',
+      'GR',
+      'GT',
+      'HK',
+      'HN',
+      'HU',
+      'ID',
+      'IE',
+      'IL',
+      'IS',
+      'IT',
+      'JP',
+      'LI',
+      'LT',
+      'LU',
+      'LV',
+      'MC',
+      'MT',
+      'MX',
+      'MY',
+      'NI',
+      'NL',
+      'NO',
+      'NZ',
+      'PA',
+      'PE',
+      'PH',
+      'PL',
+      'PT',
+      'PY',
+      'RO',
+      'SE',
+      'SG',
+      'SK',
+      'SV',
+      'TH',
+      'TR',
+      'TW',
+      'US',
+      'UY',
+      'VN',
+      'ZA'
+    ],
+    disc_number: 1,
+    duration_ms: 221826,
+    explicit: true,
+    external_ids: {
+      isrc: 'USUM71714108'
+    },
+    external_urls: {
+      spotify: 'https://open.spotify.com/track/5A4anUcHrgnqH9UorW9XbG'
+    },
+    href: 'https://api.spotify.com/v1/tracks/5A4anUcHrgnqH9UorW9XbG',
+    id: '5A4anUcHrgnqH9UorW9XbG',
+    is_local: false,
+    name: 'Big Shot (with Travis Scott)',
+    popularity: 80,
+    preview_url:
+      'https://p.scdn.co/mp3-preview/7a815e3ccfa5ed1257a67d602d013d6adcfed629?cid=774b29d4f13844c495f206cafdad9c86',
+    track_number: 13,
+    type: 'track',
+    uri: 'spotify:track:5A4anUcHrgnqH9UorW9XbG'
+  },
+  {
+    album: {
+      album_type: 'album',
+      artists: [
+        {
+          external_urls: {
+            spotify: 'https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg'
+          },
+          href: 'https://api.spotify.com/v1/artists/2YZyLoL8N0Wb9xBt1NhZWg',
+          id: '2YZyLoL8N0Wb9xBt1NhZWg',
+          name: 'Kendrick Lamar',
+          type: 'artist',
+          uri: 'spotify:artist:2YZyLoL8N0Wb9xBt1NhZWg'
+        }
+      ],
+      available_markets: [
+        'AD',
+        'AR',
+        'AT',
+        'AU',
+        'BE',
+        'BG',
+        'BO',
+        'BR',
+        'CA',
+        'CH',
+        'CL',
+        'CO',
+        'CR',
+        'CY',
+        'CZ',
+        'DE',
+        'DK',
+        'DO',
+        'EC',
+        'EE',
+        'ES',
+        'FI',
+        'FR',
+        'GB',
+        'GR',
+        'GT',
+        'HK',
+        'HN',
+        'HU',
+        'ID',
+        'IE',
+        'IL',
+        'IS',
+        'IT',
+        'JP',
+        'LI',
+        'LT',
+        'LU',
+        'LV',
+        'MC',
+        'MT',
+        'MX',
+        'MY',
+        'NI',
+        'NL',
+        'NO',
+        'NZ',
+        'PA',
+        'PE',
+        'PH',
+        'PL',
+        'PT',
+        'PY',
+        'RO',
+        'SE',
+        'SG',
+        'SK',
+        'SV',
+        'TH',
+        'TR',
+        'TW',
+        'US',
+        'UY',
+        'VN',
+        'ZA'
+      ],
+      external_urls: {
+        spotify: 'https://open.spotify.com/album/7wbJhbCvhPfbK1CLAkpq25'
+      },
+      href: 'https://api.spotify.com/v1/albums/7wbJhbCvhPfbK1CLAkpq25',
+      id: '7wbJhbCvhPfbK1CLAkpq25',
+      images: [
+        {
+          height: 640,
+          url:
+            'https://i.scdn.co/image/a6f7746f54fffaaa3feeabe167e9e669fe2fcd51',
+          width: 640
+        },
+        {
+          height: 300,
+          url:
+            'https://i.scdn.co/image/4e76f9ea1d80c1ed7bbc4e8fe625cf38d66f60cb',
+          width: 300
+        },
+        {
+          height: 64,
+          url:
+            'https://i.scdn.co/image/16556b63635740ec6241455ac6d12651af0ae276',
+          width: 64
+        }
+      ],
+      name: 'DAMN.',
+      release_date: '2017-04-14',
+      release_date_precision: 'day',
+      type: 'album',
+      uri: 'spotify:album:7wbJhbCvhPfbK1CLAkpq25'
+    },
+    artists: [
+      {
+        external_urls: {
+          spotify: 'https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg'
+        },
+        href: 'https://api.spotify.com/v1/artists/2YZyLoL8N0Wb9xBt1NhZWg',
+        id: '2YZyLoL8N0Wb9xBt1NhZWg',
+        name: 'Kendrick Lamar',
+        type: 'artist',
+        uri: 'spotify:artist:2YZyLoL8N0Wb9xBt1NhZWg'
+      }
+    ],
+    available_markets: [
+      'AD',
+      'AR',
+      'AT',
+      'AU',
+      'BE',
+      'BG',
+      'BO',
+      'BR',
+      'CA',
+      'CH',
+      'CL',
+      'CO',
+      'CR',
+      'CY',
+      'CZ',
+      'DE',
+      'DK',
+      'DO',
+      'EC',
+      'EE',
+      'ES',
+      'FI',
+      'FR',
+      'GB',
+      'GR',
+      'GT',
+      'HK',
+      'HN',
+      'HU',
+      'ID',
+      'IE',
+      'IL',
+      'IS',
+      'IT',
+      'JP',
+      'LI',
+      'LT',
+      'LU',
+      'LV',
+      'MC',
+      'MT',
+      'MX',
+      'MY',
+      'NI',
+      'NL',
+      'NO',
+      'NZ',
+      'PA',
+      'PE',
+      'PH',
+      'PL',
+      'PT',
+      'PY',
+      'RO',
+      'SE',
+      'SG',
+      'SK',
+      'SV',
+      'TH',
+      'TR',
+      'TW',
+      'US',
+      'UY',
+      'VN',
+      'ZA'
+    ],
+    disc_number: 1,
+    duration_ms: 185946,
+    explicit: true,
+    external_ids: {
+      isrc: 'USUM71703079'
+    },
+    external_urls: {
+      spotify: 'https://open.spotify.com/track/4cGeMgLcykDLAazHFpbZbU'
+    },
+    href: 'https://api.spotify.com/v1/tracks/4cGeMgLcykDLAazHFpbZbU',
+    id: '4cGeMgLcykDLAazHFpbZbU',
+    is_local: false,
+    name: 'DNA.',
+    popularity: 79,
+    preview_url:
+      'https://p.scdn.co/mp3-preview/935959f38fea2bde6fb730472433916118e56319?cid=774b29d4f13844c495f206cafdad9c86',
+    track_number: 2,
+    type: 'track',
+    uri: 'spotify:track:4cGeMgLcykDLAazHFpbZbU'
+  },
+  {
+    album: {
+      album_type: 'album',
+      artists: [
+        {
+          external_urls: {
+            spotify: 'https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg'
+          },
+          href: 'https://api.spotify.com/v1/artists/2YZyLoL8N0Wb9xBt1NhZWg',
+          id: '2YZyLoL8N0Wb9xBt1NhZWg',
+          name: 'Kendrick Lamar',
+          type: 'artist',
+          uri: 'spotify:artist:2YZyLoL8N0Wb9xBt1NhZWg'
+        },
+        {
+          external_urls: {
+            spotify: 'https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ'
+          },
+          href: 'https://api.spotify.com/v1/artists/1Xyo4u8uXC1ZmMpatF05PJ',
+          id: '1Xyo4u8uXC1ZmMpatF05PJ',
+          name: 'The Weeknd',
+          type: 'artist',
+          uri: 'spotify:artist:1Xyo4u8uXC1ZmMpatF05PJ'
+        },
+        {
+          external_urls: {
+            spotify: 'https://open.spotify.com/artist/7tYKF4w9nC0nq9CsPZTHyP'
+          },
+          href: 'https://api.spotify.com/v1/artists/7tYKF4w9nC0nq9CsPZTHyP',
+          id: '7tYKF4w9nC0nq9CsPZTHyP',
+          name: 'SZA',
+          type: 'artist',
+          uri: 'spotify:artist:7tYKF4w9nC0nq9CsPZTHyP'
+        }
+      ],
+      available_markets: [
+        'AD',
+        'AR',
+        'AT',
+        'AU',
+        'BE',
+        'BG',
+        'BO',
+        'BR',
+        'CA',
+        'CH',
+        'CL',
+        'CO',
+        'CR',
+        'CY',
+        'CZ',
+        'DE',
+        'DK',
+        'DO',
+        'EC',
+        'EE',
+        'ES',
+        'FI',
+        'FR',
+        'GB',
+        'GR',
+        'GT',
+        'HK',
+        'HN',
+        'HU',
+        'ID',
+        'IE',
+        'IL',
+        'IS',
+        'IT',
+        'JP',
+        'LI',
+        'LT',
+        'LU',
+        'LV',
+        'MC',
+        'MT',
+        'MX',
+        'MY',
+        'NI',
+        'NL',
+        'NO',
+        'NZ',
+        'PA',
+        'PE',
+        'PH',
+        'PL',
+        'PT',
+        'PY',
+        'RO',
+        'SE',
+        'SG',
+        'SK',
+        'SV',
+        'TH',
+        'TR',
+        'TW',
+        'US',
+        'UY',
+        'VN',
+        'ZA'
+      ],
+      external_urls: {
+        spotify: 'https://open.spotify.com/album/5f8pMn2A5d5lKMDapYbCmp'
+      },
+      href: 'https://api.spotify.com/v1/albums/5f8pMn2A5d5lKMDapYbCmp',
+      id: '5f8pMn2A5d5lKMDapYbCmp',
+      images: [
+        {
+          height: 640,
+          url:
+            'https://i.scdn.co/image/dfc8859cfa4ac0a761742f6b64e45f4ea65ece50',
+          width: 640
+        },
+        {
+          height: 300,
+          url:
+            'https://i.scdn.co/image/5a722077548b7d48abfd93324592026337789a8e',
+          width: 300
+        },
+        {
+          height: 64,
+          url:
+            'https://i.scdn.co/image/955b112723eae662639f2322054b7a1cb8f4ad1a',
+          width: 64
+        }
+      ],
+      name: 'Black Panther The Album Music From And Inspired By',
+      release_date: '2018-02-09',
+      release_date_precision: 'day',
+      type: 'album',
+      uri: 'spotify:album:5f8pMn2A5d5lKMDapYbCmp'
+    },
+    artists: [
+      {
+        external_urls: {
+          spotify: 'https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg'
+        },
+        href: 'https://api.spotify.com/v1/artists/2YZyLoL8N0Wb9xBt1NhZWg',
+        id: '2YZyLoL8N0Wb9xBt1NhZWg',
+        name: 'Kendrick Lamar',
+        type: 'artist',
+        uri: 'spotify:artist:2YZyLoL8N0Wb9xBt1NhZWg'
+      }
+    ],
+    available_markets: [
+      'AD',
+      'AR',
+      'AT',
+      'AU',
+      'BE',
+      'BG',
+      'BO',
+      'BR',
+      'CA',
+      'CH',
+      'CL',
+      'CO',
+      'CR',
+      'CY',
+      'CZ',
+      'DE',
+      'DK',
+      'DO',
+      'EC',
+      'EE',
+      'ES',
+      'FI',
+      'FR',
+      'GB',
+      'GR',
+      'GT',
+      'HK',
+      'HN',
+      'HU',
+      'ID',
+      'IE',
+      'IL',
+      'IS',
+      'IT',
+      'JP',
+      'LI',
+      'LT',
+      'LU',
+      'LV',
+      'MC',
+      'MT',
+      'MX',
+      'MY',
+      'NI',
+      'NL',
+      'NO',
+      'NZ',
+      'PA',
+      'PE',
+      'PH',
+      'PL',
+      'PT',
+      'PY',
+      'RO',
+      'SE',
+      'SG',
+      'SK',
+      'SV',
+      'TH',
+      'TR',
+      'TW',
+      'US',
+      'UY',
+      'VN',
+      'ZA'
+    ],
+    disc_number: 1,
+    duration_ms: 130613,
+    explicit: true,
+    external_ids: {
+      isrc: 'USUM71714096'
+    },
+    external_urls: {
+      spotify: 'https://open.spotify.com/track/3vKU0GTHo5nl3kTCYAiqc3'
+    },
+    href: 'https://api.spotify.com/v1/tracks/3vKU0GTHo5nl3kTCYAiqc3',
+    id: '3vKU0GTHo5nl3kTCYAiqc3',
+    is_local: false,
+    name: 'Black Panther',
+    popularity: 77,
+    preview_url:
+      'https://p.scdn.co/mp3-preview/b64fe2a94b5093ee5e694aff06aeae30afcc7fa2?cid=774b29d4f13844c495f206cafdad9c86',
+    track_number: 1,
+    type: 'track',
+    uri: 'spotify:track:3vKU0GTHo5nl3kTCYAiqc3'
+  }
+]
--- client/src/sections/battle.js
+++ client/src/sections/battle.js
@@ -1,4 +1,4 @@
-import { showSection } from '../section-handler'
+import { showSection, getSectionElement } from '../section-handler'
 import { go } from '../browser-router'
 
 import fakeArtists from '../dev-json/artists'
@@ -7,6 +7,11 @@ import fakeTracks from '../dev-json/tracks'
 export default class Battle {
   artistNameEl = document.querySelector('[data-artist-name]')
   artistImageEl = document.querySelector('[data-artist-image]')
+  songAEl = document.querySelector('[data-battle-song="a"]')
+  songBEl = document.querySelector('[data-battle-song="b"]')
+
+  artist = null
+  tracks = []
 
   constructor() {
     const artist = window.history.state
@@ -20,6 +25,24 @@ export default class Battle {
 
       this.fetchArtist(artistId)
     }
+
+    this.onSongSelect = this.onSongSelect.bind(this)
+
+    this.songAEl
+      .querySelector('[data-select-song]')
+      .addEventListener('click', this.onSongSelect)
+    this.songBEl
+      .querySelector('[data-select-song]')
+      .addEventListener('click', this.onSongSelect)
+  }
+
+  deactivate() {
+    this.songAEl
+      .querySelector('[data-select-song]')
+      .removeEventListener('click', this.onSongSelect)
+    this.songBEl
+      .querySelector('[data-select-song]')
+      .removeEventListener('click', this.onSongSelect)
   }
 
   async fetchArtist(artistId) {
@@ -32,12 +55,7 @@ export default class Battle {
       }, 300)
     )
 
-    try {
-      const artist = await fakeRequest
-      this.setArtist(artist)
-    } catch (exception) {
-      go('/')
-    }
+    fakeRequest.then(artist => this.setArtist(artist), () => go('/'))
   }
 
   async setArtist(artist) {
@@ -58,9 +76,69 @@ export default class Battle {
   initializeBattle(tracks) {
     showSection('battle')
 
+    this.tracks = tracks
+    this.currentTracks = [...tracks]
+    this.stages = [[]]
+
     this.artistNameEl.innerHTML = this.artist.name
     this.artistImageEl.src = this.artist.images.reduce(
       (res, cur) => (cur.width < res.width ? cur : res)
     ).url
+
+    this.nextBattle()
+  }
+
+  nextBattle() {
+    const songA = this.currentTracks.shift()
+    const songB = this.currentTracks.shift()
+
+    this.renderSong(songA, this.songAEl)
+    this.renderSong(songB, this.songBEl)
+  }
+
+  renderSong(song, songEl) {
+    songEl.setAttribute('data-song-id', song.id)
+
+    songEl.querySelector('[data-song-name]').innerHTML = song.name
+
+    songEl.querySelector('[data-song-image]').src = song.album.images.find(
+      image => image.width === 300
+    ).url
+
+    songEl.querySelector('[data-song-audio]').src = song.preview_url
+  }
+
+  onSongSelect(event) {
+    const songEl = event.target.closest('[data-song-id]')
+    const songId = songEl.dataset.songId
+
+    const song = this.tracks.find(song => song.id === songId)
+
+    const currentStage = this.stages[this.stages.length - 1]
+    currentStage.push(song)
+
+    if (this.currentTracks.length === 0) {
+      if (currentStage.length === 1) {
+        return this.showWinner()
+      } else {
+        this.currentTracks = [...currentStage]
+        this.stages.push([])
+      }
+    }
+
+    this.nextBattle()
+  }
+
+  showWinner() {
+    showSection('winner')
+
+    const winner = this.stages[this.stages.length - 1][0]
+    const winnerEl = getSectionElement('winner')
+
+    winnerEl.querySelector('[data-song-image]').src = winner.album.images.find(
+      image => image.width === 640
+    ).url
+
+    winnerEl.querySelector('[data-song-name]').innerHTML = winner.name
   }
 }
